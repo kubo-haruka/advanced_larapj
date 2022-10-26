dfb675e97c07a08bc92c623adbc6f92451dc87a0
kubo-haruka <rupixx28@gmail.com>
2022-10-26 20:46:36 +0900

    wip

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use App\Http\Requests\AuthorRequest;
+
+class AuthorController extends Controller
+{
+		public function index()
+    {
+        $authors = Author::Paginate(4);
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function add()
+    {
+        return view('add');
+    }
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+
+		public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+    public function search(Request $request)
+    {
+        $author = Author::where('name', 'LIKE BINARY',"%{$request->input}%")->first();
+        $param = [
+            'input' => $request->input,
+            'author' => $author
+        ];
+        return view('find', $param);
+    }
+
+    public function edit(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+    public function update(Request $request)
+    {
+        $this->validate($request, Author::$rules);
+        $form = $request->all();
+        unset($form['_token']);
+        Author::where('id', $request->id)->update($form);
+        return redirect('/');
+    }
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delete', ['form' => $author]);
+    }
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
+    public function bind(Author $author)
+    {
+        $data = [
+            'author' => $author,
+        ];
+        return view('author.binds', $data);
+    }
+    public function get()
+    {
+        $text = [
+            'content' => '自由に入力してください',
+        ];
+        return view('middleware', $text);
+    }
+    public function post(Request $request)
+    {
+        $content = $request->content;
+        $text = [
+            'content' => $content . 'と入力しましたね'
+        ];
+        return view('middleware', $text);
+    }
+
+    public function relate(Request $request)
+    {
+        $hasbooks = Author::has('book')->get();
+        $nobooks = Author::doesntHave('book')->get();
+        $param = ['hasbooks' => $hasbooks, 'nobooks' => $nobooks];
+        return view('author.index',$param);
+    }
+}
